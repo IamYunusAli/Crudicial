0dc3707991ac1c9a159da0363254db8fce29f967
Yunus Ali <aliyunus291@gmail.com>
2023-02-23 15:10:54 +0300

    remove credentials

--- src/createClient.js
+++ src/createClient.js
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const myClient= createClient(
-'https://gvipzmxdsznclspclczv.supabase.co',
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2aXB6bXhkc3puY2xzcGNsY3p2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5Njg4OTAsImV4cCI6MTk4NTU0NDg5MH0.R4fYj0CkN-iNIRkU1yv-aod0eivt11fTUd_uFo2UQgw'
 
 )
